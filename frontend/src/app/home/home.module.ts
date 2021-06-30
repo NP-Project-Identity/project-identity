16cb77953038f81c1ec34688b04c761ce42dce3c
dodieboy <dodieboy@users.noreply.github.com>
2021-06-30 13:40:39 +0800

    fix: UI problem for mobile view

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
+import {ComponentsModule} from '../components.module';
+import {HomePageRoutingModule} from './home-routing.module';
+import {HomePage} from './home.page';
 
-import { IonicModule } from '@ionic/angular';
 
-import { HomePageRoutingModule } from './home-routing.module';
 
-import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ComponentsModule
   ],
   declarations: [HomePage]
 })
