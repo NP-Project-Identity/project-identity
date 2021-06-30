afd76795f74a9ed1f981b5b3878bf0dc68ea82bd
dodieboy <dodieboy@users.noreply.github.com>
2021-06-30 13:40:39 +0800

    fix: UI problem for mobile view

--- frontend/src/app/learningmgtplatform/learningmgtplatform.module.ts
+++ frontend/src/app/learningmgtplatform/learningmgtplatform.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
+import {ComponentsModule} from '../components.module';
+import {LearningmgtplatformPageRoutingModule} from './learningmgtplatform-routing.module';
+import {LearningmgtplatformPage} from './learningmgtplatform.page';
 
-import { IonicModule } from '@ionic/angular';
 
-import { LearningmgtplatformPageRoutingModule } from './learningmgtplatform-routing.module';
 
-import { LearningmgtplatformPage } from './learningmgtplatform.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LearningmgtplatformPageRoutingModule
+    LearningmgtplatformPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [LearningmgtplatformPage]
 })
