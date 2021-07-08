153773e52a7b1546955bdd9cac9c1b05499a3940
dodieboy <dodieboy@users.noreply.github.com>
2021-07-08 10:15:12 +0800

    feat: learningmgtplatform

--- frontend/src/app/components.module.ts
+++ frontend/src/app/components.module.ts
@@ -2,12 +2,13 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {IonicModule} from '@ionic/angular';
+import {ForumCatComponent} from './forum-cat/forum-cat.component';
 import {MainHeaderComponent} from './main-header/main-header.component';
 import {SubbarComponent} from './subbar/subbar.component';
 
 @NgModule({
-  declarations: [MainHeaderComponent, SubbarComponent],
-  exports: [MainHeaderComponent, SubbarComponent],
+  declarations: [MainHeaderComponent, SubbarComponent, ForumCatComponent],
+  exports: [MainHeaderComponent, SubbarComponent, ForumCatComponent],
   imports: [IonicModule, RouterModule, CommonModule],
 })
 export class ComponentsModule { }
