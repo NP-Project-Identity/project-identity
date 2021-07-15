dc72073b7c2fc9464b9d43e854393d68412a3161
Jun Hao Lim <s10195387@connect.np.edu.sg>
2021-07-15 10:52:19 +0800

    added infinitescroll + news

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonInfiniteScroll} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
   navItems = [
     { title: 'Lmgtplatform', url: '../learningmgtplatform', icon: 'layers' },
     { title: 'Forum', url: '../forum', icon: 'chatbox-ellipses-outline' }
@@ -17,9 +19,52 @@ export class HomePage implements OnInit {
     {name: 'NS'},
     {name: 'ISProid'}
   ];
-  constructor() { }
+  news = [
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'},
+    {title: 'TCP', content: 'Invitation to attend 2 online Lectures by distinguished speakers: "An Innovative Economy" by Mr Ravi Menon and "The US-China Relationship: From Conflict to Resolution" by Mr Stephen Roach!'}
+  ];
+  selectednews = [
+  ];
+  newslength: number;
 
+  constructor() {
+    this.newslength = 0;
+    this.appendNews(6);
+  }
   ngOnInit() {
   }
-
-}
+  list = document.getElementById('list');
+  loadNews(event) {
+    setTimeout(() => {
+      if (this.newslength < this.news.length) {
+        console.log('Loading Data...');
+        this.appendNews(2);
+        event.target.complete();
+        console.log('done');
+      }
+      else {
+        console.log('No news left');
+        event.target.disabled = true;
+        event.target.complete();
+      }
+    }, 1000);
+  }
+  appendNews(number) {
+    const olength = this.newslength;
+    for (var i = 0; i < number; i++) {
+      this.selectednews.push(this.news[i + olength])
+      this.newslength++;
+  }
+}}
