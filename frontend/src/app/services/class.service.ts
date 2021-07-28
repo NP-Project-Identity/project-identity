c75a7db4e4011a720bc75f705e7d7592c5ad65f9
dodieboy <dodieboy@users.noreply.github.com>
2021-07-28 15:30:15 +0800

    Feat: Redesign data format for forum and class service

--- frontend/src/app/services/class.service.ts
+++ frontend/src/app/services/class.service.ts
@@ -5,12 +5,30 @@ import {Injectable} from '@angular/core';
 })
 
 export class ClassService {
-  private user: [string, string[]][] = [["S10198161", ["PROGI", "PROG2II", "EH", "CAPESTONE"]], ['S10199111', ['PRogI', 'PROG2II', 'EH', 'SSD']]];
-  private class: any[] = [["PROGI", [{title: "Announcement", type: "folder", content: [{title: "Welcome to PROG 1", desc: "Welcome back to the School of ICT and hope that you had a good break.", type: "text"}]}, {title: "Week 1", type: "folder", content: [{title: "Week 1 ", desc: "Welcome back to the School of ICT and hope that you had a good break.", type: "text"}, {title: "resource", desc: "File need to week 1 lesson", type: "folder"}]}, {title: "Week 2", type: "folder"}]]];
+  private user: any[] = [
+    {id: "S10198161", lesson: ["PROGI", "PROG2II", "EH", "CAPESTONE"]},
+    {id: 'S10199111', lesson: ['PRogI', 'PROG2II', 'EH', 'SSD']}
+  ];
+  private class: any[] = [
+    ["PROGI", [
+      {
+        title: "Announcement", type: "folder", content: [
+          {title: "Welcome to PROG 1", desc: "Welcome back to the School of ICT and hope that you had a good break.", type: "text"}
+        ]
+      },
+      {
+        title: "Week 1", type: "folder", content: [
+          {title: "Week 1 ", desc: "Welcome back to the School of ICT and hope that you had a good break.", type: "text"},
+          {title: "resource", desc: "File need to week 1 lesson", type: "folder"}
+        ]
+      },
+      {title: "Week 2", type: "folder"}
+    ]]
+  ];
   private classContent = [[]]
 
   loadClassList(id: string): string[] {
-    return (this.user.find(el => el[0] === id)[1]);
+    return (this.user.find(el => el.id === id).lesson);
   }
   LoadClass(classID: string) {
     return (this.class.find(el => el[0] === classID)[1]);
