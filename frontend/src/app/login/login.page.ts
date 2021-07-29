9333f1fc80aeb0dba56c64e71f697b5458aa403b
dodieboy <dodieboy@users.noreply.github.com>
2021-07-29 15:28:40 +0800

    feat: login

--- frontend/src/app/login/login.page.ts
+++ frontend/src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
 import {MenuController, NavController} from '@ionic/angular';
+import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,23 @@ import {MenuController, NavController} from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm = this.formBuilder.group({
+    email: '',
+    password: ''
+  });
 
-  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController, private formBuilder: FormBuilder, private userService: UserService, private router: Router) {
     this.menuCtrl.enable(false)
   }
 
   ngOnInit() {
   }
 
+  submit() {
+    let result: Boolean = this.userService.setUser(this.loginForm.get("email").value, this.loginForm.get("password").value)
+    if (result) {
+      this.menuCtrl.enable(true)
+      this.router.navigate(['./home']);
+    }
+  }
 }
