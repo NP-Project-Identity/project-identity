3fe35e5e698dd971506e50de5e010d60f0ee737d
dodieboy <dodieboy@users.noreply.github.com>
2021-07-31 22:55:31 +0800

    Feat: pet action layout

--- frontend/src/app/pet/pet.page.ts
+++ frontend/src/app/pet/pet.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pet',
@@ -11,5 +11,4 @@ export class PetPage implements OnInit {
 
   ngOnInit() {
   }
-
 }
