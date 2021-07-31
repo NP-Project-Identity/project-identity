af27d257ac16e579bfcf4f7550e1e8243437d66a
dodieboy <dodieboy@users.noreply.github.com>
2021-07-31 15:49:59 +0800

    Feat: login validation

--- frontend/src/app/login/login.page.ts
+++ frontend/src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {MenuController, NavController} from '@ionic/angular';
 import {UserService} from '../services/user.service';
@@ -10,9 +10,14 @@ import {UserService} from '../services/user.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  isSubmitted = false;
+  errorMsg = "";
   loginForm = this.formBuilder.group({
-    email: '',
-    password: ''
+    password: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ]))
   });
 
   constructor(public navCtrl: NavController, public menuCtrl: MenuController, private formBuilder: FormBuilder, private auth: UserService, private router: Router) {
@@ -23,9 +28,30 @@ export class LoginPage implements OnInit {
   }
 
   submit() {
-    let result = this.auth.login(this.loginForm.get("email").value, this.loginForm.get("password").value)
-    if (result) {
-      this.menuCtrl.enable(true)
+    if (this.loginForm.valid) {
+      let result = this.auth.login(this.loginForm.get("email").value, this.loginForm.get("password").value)
+      if (result) {
+        this.menuCtrl.enable(true);
+      }
     }
+    else {
+      console.log(this.formError);
+      if (this.formError.email.errors?.required) {
+        this.isSubmitted = true;
+        this.errorMsg = "Email is required";
+      }
+      else if (this.formError.email.errors?.pattern) {
+        this.isSubmitted = true;
+        this.errorMsg = "Invaild email";
+      }
+      else if (this.formError.password.errors?.required) {
+        this.isSubmitted = true;
+        this.errorMsg = "Password is required";
+      }
+    }
+  }
+
+  get formError() {
+    return this.loginForm.controls;
   }
 }
