6c1e722dd2aa08b529999327d8200d6114394034
dodieboy <dodieboy@users.noreply.github.com>
2021-08-11 22:52:06 +0800

    Feat: added what this img quiz

--- frontend/src/app/quiz/quiz.page.ts
+++ frontend/src/app/quiz/quiz.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 import {QuizService} from '../services/quiz.service';
 
@@ -9,15 +10,21 @@ import {QuizService} from '../services/quiz.service';
 })
 export class QuizPage implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute, private quizService: QuizService) { }
+  constructor(private activatedRoute: ActivatedRoute, private quizService: QuizService, private formBuilder: FormBuilder) { }
 
+  public quizType: string;
   public quizTitle: string;
   public quizOption: string[] = [];
+  public quizImg: string;
   public title: string;
   public isAnswerShowing = false;
   public result = false;
   private quiz: any;
   private selectedQuiz: any;
+
+  quizForm = this.formBuilder.group({
+    answer: ''
+  });
 
   getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -26,13 +33,23 @@ export class QuizPage implements OnInit {
   ngOnInit() {
     this.quiz = this.quizService.crateQuiz(this.activatedRoute.snapshot.parent.parent.paramMap.get('id'));
     this.selectedQuiz = this.quiz[this.getRandomInt(this.quiz.length)];
+    this.quizType = this.selectedQuiz.type;
     this.quizTitle = this.selectedQuiz.question;
-    this.quizOption = this.selectedQuiz.option;
+    if (this.quizType == "mcq")
+      this.quizOption = this.selectedQuiz.option;
+    else if (this.quizType == "img") {
+      this.quizImg = "./assets/game/quiz/" + this.selectedQuiz.src;
+    }
   }
   quizAns(ans: any) {
     if (ans == this.selectedQuiz.answer) {
       this.result = true;
     }
     this.isAnswerShowing = true;
+  }
+
+  submit() {
+    if (this.quizForm.get("answer").value != null)
+      this.quizAns(this.quizForm.get("answer").value.toLowerCase());
   }
 }
