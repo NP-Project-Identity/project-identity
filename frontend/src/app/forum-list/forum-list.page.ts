52d8d17083f982b9c7523212a140fca7b5eab1b5
dodieboy <dodieboy@users.noreply.github.com>
2021-08-11 22:51:25 +0800

    Fix: cannot enter forum when sudden quest due to resource error

--- frontend/src/app/forum-list/forum-list.page.ts
+++ frontend/src/app/forum-list/forum-list.page.ts
@@ -11,7 +11,7 @@ import {UserService} from '../services/user.service';
 export class ForumListPage implements OnInit {
   public title: string;
   public list: any;
-  private mission: any = {id: "-1", title: "You got a sudden quest", content: [{text: "Enter the quest to earn currency", user: "GOD", date: "Unknown", like: "-1"}]};
+  private mission: any = {id: "-1", title: "<<You got a sudden quest>>", content: [{text: "Enter the quest to earn currency", user: "GOD", date: "Unknown", like: "-1"}]};
 
   navItems = [
     {title: 'Lmgtplatform', url: '/learningmgtplatform', icon: 'layers'},
@@ -27,7 +27,7 @@ export class ForumListPage implements OnInit {
   ngOnInit() {
     this.title = this.activatedRoute.snapshot.paramMap.get('id');
     this.list = this.forumService.loadList(this.title);
-    if (this.getRandomInt(50) == this.getRandomInt(2)) { //appear rate is 1/100 try
+    if (this.getRandomInt(2) == this.getRandomInt(2)) { //appear rate is 1/100 try
       this.list.splice(this.getRandomInt(this.list.length), 0, this.mission);
     }
   }
